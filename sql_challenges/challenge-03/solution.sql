59bd34b3a974de06eaa0451ccd8c4355d22a826d
Santiago Alonzo <jalonzo9878@gmail.com>
2026-02-26 08:28:12 -0600

    challenge03

---
+++ sql_challenges/challenge-03/solution.sql
@@ -0,0 +1,27 @@
+--lesson 10
+SELECT *, MAX(years_employed) FROM employees ;
+SELECT *, AVG(years_employed) FROM employees group by role;
+SELECT *, SUM(years_employed) FROM employees group by building;
+
+--lesson 11
+SELECT COUNT(*) FROM employees WHERE role = "Artist";
+SELECT role, COUNT(*) FROM employees GROUP BY role;
+SELECT role, SUM(years_employed) FROM employees WHERE role = "Engineer";
+
+--freesql oracle
+--try it 1
+SELECT COUNT(distinct shape) AS number_of_shapes,
+       Stddev(distinct weight) AS distinct_weight_stddev
+FROM   bricks;
+
+--try it 2
+SELECT shape, SUM(weight) AS shape_weight
+FROM   bricks
+GROUP BY shape
+ORDER BY shape ASC;
+
+--try it 3
+SELECT shape, SUM(weight)
+FROM   bricks
+GROUP  BY shape
+HAVING SUM(weight) < 4;
